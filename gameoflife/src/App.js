87dfb406b4a52ca54586499ff793f76113ac2aec
anthonyvigz <anthonyvigz@gmail.com>
2020-06-19 14:47:59 -0400

    initialized state grid

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+const numRows = 50;
+const numCols = 50;
+
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+
+    return rows;
+  });
+
+  console.log(grid);
+
   return <div className="App"></div>;
 }
 
