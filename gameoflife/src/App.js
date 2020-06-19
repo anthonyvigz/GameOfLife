c9f1fb54c1e55f40c0754310d3f23ed99b30c548
anthonyvigz <anthonyvigz@gmail.com>
2020-06-19 15:01:01 -0400

    created changing running ref variable so callback can access current value

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useRef } from "react";
 import "./App.css";
 import produce from "immer";
 
@@ -15,35 +15,56 @@ function App() {
     return rows;
   });
 
-  console.log(grid);
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef();
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    // simulate
+    setTimeout(runSimulation, 1000);
+  }, []);
 
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: `repeat(${numCols}, 20px`,
-      }}
-    >
-      {grid.map((rows, i) =>
-        rows.map((col, k) => (
-          <div
-            key={`${i}-${k}`}
-            onClick={() => {
-              const newGrid = produce(grid, (gridCopy) => {
-                gridCopy[i][k] = grid[i][k] ? 0 : 1;
-              });
-              setGrid(newGrid);
-            }}
-            style={{
-              width: 20,
-              height: 20,
-              backgroundColor: grid[i][k] ? "pink" : undefined,
-              border: "solid 1px black",
-            }}
-          />
-        ))
-      )}
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running);
+        }}
+      >
+        {running ? "stop" : "start"}
+      </button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 20px`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? "pink" : undefined,
+                border: "solid 1px black",
+              }}
+            />
+          ))
+        )}
+      </div>
+    </>
   );
 }
 
