9c0e0dac3b3c72c52f0ba9bfb53a39af677e3865
anthonyvigz <anthonyvigz@gmail.com>
2020-06-20 15:15:35 -0400

    setting up title page

--- gameoflife/src/components/Enter.jsx
+++ gameoflife/src/components/Enter.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "../styling/enter.scss";
 
 function Enter() {
-  return <div className="enterScreen">enter</div>;
+  return (
+    <div className="enterScreen">
+      <h1>Conway's Game of Life</h1>
+      <h2>Interpretation by Anthony Vigliotta</h2>
+    </div>
+  );
 }
 export default Enter;
